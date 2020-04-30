b6a78a958bcc158858274b42a293802683db6e02
ting22725 <62133455+ting22725@users.noreply.github.com>
2020-04-30 21:37:24 +0800

    Update settings.py

--- st2-master/students/settings.py
+++ st2-master/students/settings.py
@@ -35,9 +35,8 @@ from linebot.models import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
-# 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_ACCESS_TOKEN ='OBhQklMkSyOYgx8gXq/VSKAUSUEKOhkLFmAgUf0UweLcdTIQHM5EKEmWOklFWYlRwo00o24SwfpO5CvH86P3WQTYeHCwIgxcwcBWJ2Ic17Yu69KR9G2/b/RFvvPjutRdW/igsjAb1oyQClcB/WQL/gdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET='fbf8bf149c171ef4aa8cd1f534f9ba51'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
